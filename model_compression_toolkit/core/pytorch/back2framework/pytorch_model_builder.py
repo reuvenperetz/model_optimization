ace2e39db86b7dacb7ae42289289d6f0a089cc56
Reuven Peretz <reuvenp@altair-semi.com>
2022-08-25 15:31:08 +0300

    Fix remains

--- model_compression_toolkit/core/pytorch/back2framework/pytorch_model_builder.py
+++ model_compression_toolkit/core/pytorch/back2framework/pytorch_model_builder.py
@@ -239,6 +239,7 @@ class PytorchModel(torch.nn.Module):
 
         Args:
             node: The corresponding node of the layer it runs.
+            configurable_nodes_names: A list of names of configurable nodes in the quantized model.
 
         Returns: Module/functional to apply to the input tensors.
 
