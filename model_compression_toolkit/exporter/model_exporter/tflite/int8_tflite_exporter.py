078739b40da4ca518bf79f7c511d4c58d2c1b88d
reuvenp <reuvenp@altair-semi.com>
2023-01-30 01:03:00 +0200

    TFLite int8 exporter

---
+++ model_compression_toolkit/exporter/model_exporter/tflite/int8_tflite_exporter.py
@@ -0,0 +1,176 @@
+# Copyright 2022 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import copy
+from typing import Callable
+
+import keras.models
+import numpy as np
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Dense, Conv2D, Reshape
+from keras.models import clone_model
+
+from model_compression_toolkit import quantizers_infrastructure as qi
+from model_compression_toolkit.core.common import Logger
+from model_compression_toolkit.exporter.model_exporter.keras.fakely_quant_keras_exporter import FakelyQuantKerasExporter
+
+POINTWISE_KERNEL = 'pointwise_kernel'
+DEPTH_MULTIPLIER = 'depth_multiplier'
+DATA_FORMAT = 'data_format'
+DILATION_RATE = 'dilation_rate'
+DEPTHWISE_INITIALIZER = 'depthwise_initializer'
+DEPTHWISE_REGULARIZER = 'depthwise_regularizer'
+DEPTHWISE_CONSTRAINT = 'depthwise_constraint'
+BIAS_INITIALIZER = 'bias_initializer'
+BIAS_REGULARIZER = 'bias_regularizer'
+BIAS_CONSTRAINT = 'bias_constraint'
+ACTIVITY_REGULARIZER = 'activity_regularizer'
+KERNEL_INITIALIZER = 'kernel_initializer'
+
+
+KERNEL_REGULARIZER = 'kernel_regularizer'
+SEPARABLE_PW_KERNEL_REGULARIZER = 'pointwise_regularizer'
+KERNEL_CONSTRAINT = 'kernel_constraint'
+SEPARABLE_PW_KERNEL_CONSTRAINT = 'pointwise_constraint'
+# Layers attributes constants:
+KERNEL_SIZE = 'kernel_size'
+PADDING = 'padding'
+GROUPS = 'groups'
+STRIDES = 'strides'
+DILATIONS = 'dilation_rate'
+DATA_FORMAT = 'data_format'
+LAYER_NAME = 'name'
+TRAINABLE = 'trainable'
+ACTIVATION = 'activation'
+USE_BIAS = 'use_bias'
+FILTERS = 'filters'
+UNITS = 'units'
+PAD_VALID = 'valid'
+PAD_SAME = 'same'
+RELU_MAX_VALUE = 'max_value'
+THRESHOLD = 'threshold'
+NEGATIVE_SLOPE = 'negative_slope'
+CHANNELS_FORMAT = 'data_format'
+CHANNELS_FORMAT_FIRST = 'channels_first'
+CHANNELS_FORMAT_LAST = 'channels_last'
+AXES = 'axes'
+AXIS = 'axis'
+DIMS = 'dims'
+TARGET_SHAPE = 'target_shape'
+TRANSPOSE_A = 'transpose_a'
+TRANSPOSE_B = 'transpose_b'
+
+class INT8TFLiteExporter(FakelyQuantKerasExporter):
+    """
+    Exporter for fakely-quant TFLite models.
+    The exporter expects to receive an exportable model (where each layer's full quantization parameters
+    can be retrieved), and convert it into a fakely-quant model (namely, weights that are in fake-quant
+    format) and fake-quant layers for the activations.
+    """
+
+    def __init__(self,
+                 model: keras.models.Model,
+                 is_layer_exportable_fn: Callable,
+                 save_model_path: str):
+        """
+
+        Args:
+            model: Model to export.
+            is_layer_exportable_fn: Callable to check whether a layer can be exported or not.
+            save_model_path: Path to save the exported model.
+        """
+        super().__init__(model,
+                         is_layer_exportable_fn,
+                         save_model_path)
+
+        self.exported_model = None
+
+    def export(self):
+        """
+        Convert an exportable (fully-quantized) Keras model to a fakely-quant TFLite model
+        (namely, weights that are in fake-quant format) and fake-quant layers for the activations.
+
+        """
+
+        def _substitute_model(wrapped_layer):
+            assert isinstance(wrapped_layer, qi.KerasQuantizationWrapper), f'Layer must be wrapped but of type {type(wrapped_layer)}'
+
+            if isinstance(wrapped_layer.layer, Dense):
+                # pw_kernel = self._convert_dense_kernel_to_pw_kernel(wrapped_layer)
+                # List of pw attributes that should take from separable as they are.
+
+                pw_attr_list = ['name', ACTIVATION, USE_BIAS, BIAS_CONSTRAINT,
+                                BIAS_INITIALIZER, BIAS_REGULARIZER, TRAINABLE, ACTIVITY_REGULARIZER,
+                                KERNEL_INITIALIZER, KERNEL_REGULARIZER, KERNEL_CONSTRAINT]
+
+                dense_cfg = wrapped_layer.layer.get_config()
+                pw_cfg = {attr: dense_cfg[attr] for attr in pw_attr_list}
+
+                # Use more attributes that are not taken as are
+                pw_cfg.update({KERNEL_SIZE: (1, 1),
+                               STRIDES: (1, 1),
+                               PADDING: PAD_VALID,
+                               FILTERS: dense_cfg[UNITS]})
+                # pw_cfg = {}
+                pw_layer = Conv2D(**pw_cfg)
+                pw_layer.build(wrapped_layer.layer.input_shape)
+
+
+                dense_kernel = wrapped_layer.layer.kernel
+                pw_weights = []
+                pw_kernel = np.reshape(wrapped_layer.get_weights()[0], (1, 1, dense_kernel.get_shape()[0], dense_cfg[UNITS]))
+                pw_weights.append(pw_kernel)
+                if wrapped_layer.layer.use_bias:
+                    pw_bias = wrapped_layer.get_weights()[2]
+                    pw_weights.append(pw_bias)
+                # pw_kernel = dense_kernel.reshape()
+                pw_layer.set_weights(pw_weights)
+
+                kernel_quantizer_cfg = wrapped_layer.dispatcher.weight_quantizers['kernel'].get_config()
+                kernel_quantizer_cfg['channel_axis'] = 3
+                kernel_quantizer_cfg['input_num_dims'] = 4
+                quantizer_class = type(wrapped_layer.dispatcher.weight_quantizers['kernel'])
+                new_quantizer = quantizer_class(**kernel_quantizer_cfg)
+
+                new_weights_quantizer = copy.deepcopy(wrapped_layer.dispatcher.weight_quantizers)
+                new_weights_quantizer['kernel'] = new_quantizer
+
+                new_dispatcher = copy.deepcopy(wrapped_layer.dispatcher)
+                new_dispatcher.set_weight_quantizers(new_weights_quantizer)
+
+                # dense_weight_quantizers = wrapped_layer.dispatcher.weight_quantizers
+                # dense_weight_quantizers['kernel'] = dense_weight_quantizers['kernel'].from_config()
+                # pw_dispatcher.weight_quantizers
+                return Sequential([
+                    Reshape((1, 1, 1, dense_kernel.get_shape()[0])),
+                    qi.KerasQuantizationWrapper(pw_layer,
+                                                new_dispatcher)
+                    # TODO: Reshape from ((1, 1, 1, 1000)) -> (1,1000)
+                ])
+
+
+            return wrapped_layer
+
+        self.transformed_model = clone_model(self.model, clone_function=_substitute_model)
+        converter = tf.lite.TFLiteConverter.from_keras_model(self.transformed_model)
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        self.exported_model = converter.convert()
+
+        Logger.info(f'Exporting INT8 tflite model to: {self.save_model_path}')
+        with open(self.save_model_path, 'wb') as f:
+            f.write(self.exported_model)
+
+    def _convert_dense_kernel_to_pw_kernel(self, wrapped_layer:qi.KerasQuantizationWrapper):
+        pass
