b7b88660492a8ef037fb7ef868c31dd385cc43c8
reuvenp <reuvenp@altair-semi.com>
2023-10-22 19:08:54 +0300

    Revert "add LSQ algorithm for pytorch QAT (#831)"
    This reverts commit b98dd906dd8fc9a7f637960cb8a9ee6e56d11c8b.

--- model_compression_toolkit/qat/pytorch/quantizer/__init__.py
+++ model_compression_toolkit/qat/pytorch/quantizer/__init__.py
@@ -15,5 +15,3 @@
 
 import model_compression_toolkit.qat.pytorch.quantizer.ste_rounding.symmetric_ste
 import model_compression_toolkit.qat.pytorch.quantizer.ste_rounding.uniform_ste
-import model_compression_toolkit.qat.pytorch.quantizer.lsq.symmetric_lsq
-import model_compression_toolkit.qat.pytorch.quantizer.lsq.uniform_lsq
