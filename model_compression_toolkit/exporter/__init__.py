fc0791edc143e64db326cb77ee694c1a98941340
reuvenp <reuvenp@altair-semi.com>
2023-10-30 19:23:24 +0200

    test exporter

--- model_compression_toolkit/exporter/__init__.py
+++ model_compression_toolkit/exporter/__init__.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the Lxxicense.
 # ==============================================================================
 
 from model_compression_toolkit.exporter.model_exporter.keras.export_serialization_format import \
