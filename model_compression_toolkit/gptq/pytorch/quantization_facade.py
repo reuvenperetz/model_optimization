178eeb97dedfd023093cb55cae75679632d1cecf
reuvenp <reuvenp@altair-semi.com>
2025-06-25 11:16:18 +0300

    use graph builder in torch gptq facade

--- model_compression_toolkit/gptq/pytorch/quantization_facade.py
+++ model_compression_toolkit/gptq/pytorch/quantization_facade.py
@@ -30,6 +30,7 @@ from model_compression_toolkit.gptq.common.gptq_config import (
 from model_compression_toolkit.gptq.common.gptq_constants import REG_DEFAULT, LR_DEFAULT, LR_REST_DEFAULT, \
     LR_BIAS_DEFAULT, GPTQ_MOMENTUM, REG_DEFAULT_SLA
 from model_compression_toolkit.gptq.runner import gptq_runner
+from model_compression_toolkit.graph_builder.pytorch.pytorch_graph_builder import PytorchGraphBuilder
 from model_compression_toolkit.logger import Logger
 from model_compression_toolkit.metadata import create_model_metadata
 from model_compression_toolkit.target_platform_capabilities.schema.mct_current_schema import TargetPlatformCapabilities
@@ -228,10 +229,18 @@ if FOUND_TORCH:
         framework_quantization_capabilities = attach2pytorch.attach(target_platform_capabilities,
                                                              core_config.quantization_config.custom_tpc_opset_to_layer)
 
+        # TODO: remove the necessary of instanciating the graph builder
+        graph = PytorchGraphBuilder().build_graph(model=model,
+                                                  representative_dataset=representative_data_gen,
+                                                  fqc=framework_quantization_capabilities,
+                                                  linear_collapsing=core_config.quantization_config.linear_collapsing,
+                                                  residual_collapsing=core_config.quantization_config.residual_collapsing,
+                                                  relu_bound_to_power_of_2=core_config.quantization_config.relu_bound_to_power_of_2)
+
         # ---------------------- #
         # Core Runner
         # ---------------------- #
-        graph, bit_widths_config, hessian_info_service, scheduling_info = core_runner(in_model=model,
+        graph, bit_widths_config, hessian_info_service, scheduling_info = core_runner(graph=graph,
                                                                                       representative_data_gen=representative_data_gen,
                                                                                       core_config=core_config,
                                                                                       fw_impl=fw_impl,
